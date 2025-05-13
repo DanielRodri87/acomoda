9052e34690cf2e47607c355e5598f876dc91892b
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2025-05-13 14:40:38 -0300

    Implementação Inicial das telas estaticas na estrutura do NestJs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -9,4 +11,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('pagina-exemplo')
+  serveHtml(@Res() res: Response) {
+    return res.sendFile(join(__dirname, '..', 'public', 'pagina-exemplo.html'));
+  }
+  
+  // Você pode adicionar mais métodos para outras páginas
 }
